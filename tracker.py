29487e703b11ec7d3de88a4e56031ea40804b2a9
Peter Webb <plwebb0@gmail.com>
2025-04-08 22:21:11 -0400

    write to csv

--- tracker.py
+++ tracker.py
@@ -1,9 +1,19 @@
 from pathlib import path
+import csv
+from datetime import date
 
 class LogHandler:
-    def grocerylog(price, market):
+    def grocerylog(self, price, market,buyer):
+
         p = path('.Documents/grocerylog.csv')
+        today = date.today().isoformat()
         if p.exists():
-            p.open()
+            with open('grocerylog.csv',newline='') as csvfile:
+                fieldnames = ['price','market','date','buyer']
+                writer = csv.DictWriter(grocerylog.csv, fieldnames=fieldnames)
+                writer.writeheader():
+                writer.writerow({'price':price,'market':market,'date':today,'buyer':buyer})
         else:
             raise Exception("Grocery Log file not found.")
+
+        
