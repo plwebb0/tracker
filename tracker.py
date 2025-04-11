1d9eb38de1cc3498b0347449f98d90a40bb4d711
Peter Webb <plwebb0@gmail.com>
2025-04-11 00:50:02 -0400

    read and write to csv

--- tracker.py
+++ tracker.py
@@ -1,27 +1,42 @@
-from pathlib import path
+from pathlib import Path
 import csv
 from datetime import date
+import sys
 
-class LogHandler:
-    def grocerylog(self, price, market,buyer):
+p = sys.argv[0]
 
-        p = path('C://users/pwleb/Documents/grocerylog.csv')
-        today = date.today().isoformat()
-        if p.exists():
-            with open('grocerylog.csv',newline='') as csvfile:
-                grocreader = csv.reader(csvfile,delimiter=',',quotechar='"')
-                grocreader.reverse()
-                for row in range(5):
-                    print(grocreader[row])
-            
-            
-            
-            with open('grocerylog.csv',newline='') as csvfile:
-                fieldnames = ['price','market','date','buyer']
-                writer = csv.DictWriter('grocerylog.csv', fieldnames=fieldnames)
-                writer.writeheader()
-                writer.writerow({'price':price,'market':market,'date':today,'buyer':buyer})
-        else:
-            raise Exception("Grocery Log file not found.")
+dict={}
+for argcount, argument in enumerate(sys.argv):
+    dict['p{0}'.format(argcount)] = argument
 
-        
+p = dict['p2']
+m = dict['p3']
+e = dict['p4']
+today = date.today().isoformat()
+
+
+
+def grocerylog(price, market, buyer):
+
+    glog = Path('C:/Users/plweb/Documents/grocerylog.csv')
+    logpath = 'C:/Users/plweb/Documents/grocerylog.csv'
+    today = date.today().isoformat()
+    fieldDict = {'price':price,'market':market,'date':today,'buyer':buyer}
+    #new_row = [price,market,today,buyer]
+
+    if glog.exists():
+        with open(logpath,newline='') as csvfile:
+            grocfile = list(csv.reader(csvfile,delimiter=',',quotechar='"'))
+            
+            for row in grocfile:
+                print(row)
+            csvfile.close()
+            
+        with open(logpath,'a',newline='') as csvfile:
+            fieldnames = ['price','market','date','buyer']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow(fieldDict)
+    else:
+        raise Exception("Grocery Log file not found.")
+
+grocerylog(p,m,e)
