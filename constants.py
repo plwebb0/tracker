495221c7e215a5b6164beac4b45b6198847523e6
Peter Webb <plwebb0@gmail.com>
2025-04-11 23:56:31 -0400

    create constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+
+
+WINDOWS_LOGFILE_PATH = 'C:/Users/plweb/Documents/grocerylog.csv'
+LINUX_LOGFILE_PATH = '~/Documents/grocerylog.csv'
+LINUX_ARGUMENTS = [0,1,2]
+WINDOWS_ARGUMENTS = [2,3,4]
+TODAY = date.today().isoformat()
+MARKETS = ['ALDI','TRADER JOES', 'MOMS', 'WHOLE FOODS', 'TARGET', 'GIANT', 'ACME', 'WAWA', 'OTHER']
+BUYERS = ['ANNA', 'PETER', 'RORA']
+
