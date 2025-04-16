c7fa914fea7fab4ef563d2e4b3d4e01ebf95a8b2
Peter Webb <plwebb0@gmail.com>
2025-04-15 22:39:57 -0400

    move filepath to separate function

--- tracker.py
+++ tracker.py
@@ -2,9 +2,11 @@ from pathlib import Path
 import csv
 from datetime import date
 import sys
+import platform
+from . import constants
+
 
 p = sys.argv[0]
-
 dict={}
 for argcount, argument in enumerate(sys.argv):
     dict['p{0}'.format(argcount)] = argument
@@ -12,27 +14,30 @@ for argcount, argument in enumerate(sys.argv):
 p = dict['p2']
 m = dict['p3']
 e = dict['p4']
-today = date.today().isoformat()
 
 
+def getFilePath():
+    os = platform.system()
+    if os == 'Windows':
+        filepath = constants.WINDOWS_LOGFILE_PATH
+    elif os == 'Linux': 
+        filepath = constants.LINUX_LOGFILE_PATH
+    else: raise Exception("Not Windows or Linux I wasn't prepared for this")
+    return filepath
 
-def grocerylog(price, market, buyer):
 
-    glog = Path('C:/Users/plweb/Documents/grocerylog.csv')
-    logpath = 'C:/Users/plweb/Documents/grocerylog.csv'
-    today = date.today().isoformat()
-    fieldDict = {'price':price,'market':market,'date':today,'buyer':buyer}
+def grocerylog(price, market, buyer):    
+    fieldDict = {'price':price,'market':market,'date':constants.TODAY,'buyer':buyer}
     #new_row = [price,market,today,buyer]
 
-    if glog.exists():
-        with open(logpath,newline='') as csvfile:
-            grocfile = list(csv.reader(csvfile,delimiter=',',quotechar='"'))
-            
+    if Path(getFilePath).exists():
+        with open(getFilePath,newline='') as csvfile:
+            grocfile = list(csv.reader(csvfile,delimiter=',',quotechar='"'))   
             for row in grocfile:
                 print(row)
             csvfile.close()
             
-        with open(logpath,'a',newline='') as csvfile:
+        with open(getFilePath,'a',newline='') as csvfile:
             fieldnames = ['price','market','date','buyer']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow(fieldDict)
